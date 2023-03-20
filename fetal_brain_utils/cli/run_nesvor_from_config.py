1f04cf011bc0a30d375239a1857d96930a8766f4
Paul <pauldevianne@gmail.com>
2023-03-20 11:44:43 +0100

    mount datafile from container to host with magical command to grant permission

--- fetal_brain_utils/cli/run_nesvor_from_config.py
+++ fetal_brain_utils/cli/run_nesvor_from_config.py
@@ -21,7 +21,7 @@ os.environ['MKL_THREADING_LAYER'] = 'GNU'
 DATA_PATH = Path("/media/tsanchez/tsanchez_data/data/data")
 AUTO_MASK_PATH = "/media/tsanchez/tsanchez_data/data/out_anon/masks"
 
-BATCH_SIZE = 4096
+BATCH_SIZE = 2048
 
 
 def get_mask_path(bids_dir, subject, ses, run):
@@ -223,7 +223,7 @@ def iterate_subject(
                     f"--output-resolution {res} "
                     f"--output-volume {output_file} "
                     f"--output-resolution {res} "
-                    f"--inference-batch-size 4000"
+                    f"--inference-batch-size 16384"
                     f" --single-precision"
                 )
 

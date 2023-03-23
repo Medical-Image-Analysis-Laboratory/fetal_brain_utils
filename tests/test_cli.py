89c47fcee8202385c0359a8da41d1ab2047f1545
Thomas Sanchez <sanchez.thomas056@gmail.com>
2023-03-23 16:37:56 +0100

    Change stdout capture to capsys

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,6 @@
 from fetal_brain_utils.cli.run_niftymic import (
     iterate_subject as run_niftymic,
+    main as main_niftymic,
 )
 from fetal_brain_utils.cli.run_svrtk import (
     iterate_subject as run_svrtk,
@@ -11,8 +12,6 @@ from fetal_brain_utils.cli.run_nesvor_from_config import (
 import json
 from bids import BIDSLayout
 from pathlib import Path
-import io
-from contextlib import redirect_stdout
 
 
 FILE_DIR = Path(__file__).parent.resolve()
@@ -21,7 +20,7 @@ CONFIG_PATH = FILE_DIR / "data/code/params.json"
 MASKS_DIR = FILE_DIR / "data/derivatives/masks"
 
 
-def test_niftymic_iterate_subject():
+def test_niftymic_iterate_subject(capsys):
     """Test the text output of run_niftymic."""
     with open(CONFIG_PATH, "r") as f:
         params = json.load(f)
@@ -31,31 +30,29 @@ def test_niftymic_iterate_subject():
     masks_folder = MASKS_DIR
     output_path = "out"
 
-    f = io.StringIO()
-    with redirect_stdout(f):
-        run_niftymic(
-            sub,
-            config_sub,
-            CONFIG_PATH,
-            bids_layout,
-            BIDS_DIR,
-            output_path,
-            masks_folder,
-            alpha=0.068,
-            participant_label=None,
-            use_preprocessed=False,
-            fake_run=True,
-        )
-    out = f.getvalue()
+    run_niftymic(
+        sub,
+        config_sub,
+        CONFIG_PATH,
+        bids_layout,
+        BIDS_DIR,
+        output_path,
+        masks_folder,
+        alpha=0.068,
+        participant_label=None,
+        use_preprocessed=False,
+        fake_run=True,
+    )
+    captured = capsys.readouterr()
 
     with open(FILE_DIR / "output/niftymic_out.txt") as f:
         niftymic = f.readlines()
     niftymic = "".join(niftymic).replace("<PATH>", str(FILE_DIR))
 
-    assert out == niftymic
+    assert captured.out == niftymic
 
 
-def test_svrtk_iterate_subject():
+def test_svrtk_iterate_subject(capsys):
     """Test the text output of run_svrtk."""
     with open(CONFIG_PATH, "r") as f:
         params = json.load(f)
@@ -65,28 +62,26 @@ def test_svrtk_iterate_subject():
     masks_folder = MASKS_DIR
     output_path = "out"
 
-    f = io.StringIO()
-    with redirect_stdout(f):
-        run_svrtk(
-            sub,
-            config_sub,
-            CONFIG_PATH,
-            bids_layout,
-            BIDS_DIR,
-            output_path,
-            masks_folder,
-            participant_label=None,
-            fake_run=True,
-        )
-    out = f.getvalue()
+    run_svrtk(
+        sub,
+        config_sub,
+        CONFIG_PATH,
+        bids_layout,
+        BIDS_DIR,
+        output_path,
+        masks_folder,
+        participant_label=None,
+        fake_run=True,
+    )
+    captured = capsys.readouterr()
     with open(FILE_DIR / "output/svrtk_out.txt") as f:
         svrtk = f.readlines()
     svrtk = "".join(svrtk).replace("<PATH>", str(FILE_DIR))
 
-    assert out == svrtk
+    assert captured.out == svrtk
 
 
-def test_nesvor_source_iterate_subject():
+def test_nesvor_source_iterate_subject(capsys):
     """Test the text output of run_nesvor_from_config."""
     with open(CONFIG_PATH, "r") as f:
         params = json.load(f)
@@ -96,24 +91,21 @@ def test_nesvor_source_iterate_subject():
     masks_folder = MASKS_DIR
     output_path = "out"
 
-    f = io.StringIO()
-    with redirect_stdout(f):
-        run_nesvor(
-            sub,
-            config_sub,
-            bids_layout,
-            BIDS_DIR,
-            output_path,
-            masks_folder,
-            participant_label=None,
-            target_res=[1.1],
-            single_precision=False,
-            config=params,
-            fake_run=True,
-        )
-    out = f.getvalue()
-    print(out)
+    run_nesvor(
+        sub,
+        config_sub,
+        bids_layout,
+        BIDS_DIR,
+        output_path,
+        masks_folder,
+        participant_label=None,
+        target_res=[1.1],
+        single_precision=False,
+        config=params,
+        fake_run=True,
+    )
+    captured = capsys.readouterr()
     with open(FILE_DIR / "output/nesvor_source_out.txt") as f:
         nesvor = f.readlines()
     nesvor = "".join(nesvor).replace("<PATH>", str(FILE_DIR))
-    assert out == nesvor
+    assert captured.out == nesvor

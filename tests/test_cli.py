812d6e4ebeb19006450a99673d1ca005ff3eeb47
Thomas Sanchez <sanchez.thomas056@gmail.com>
2023-03-24 13:49:59 +0100

    Modified iterate_subject tests to run directly from the main

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -29,6 +29,7 @@ FILE_DIR = Path(__file__).parent.resolve()
 BIDS_DIR = FILE_DIR / "data"
 CONFIG_PATH = FILE_DIR / "data/code/params.json"
 MASKS_DIR = FILE_DIR / "data/derivatives/masks"
+OUT_PATH = FILE_DIR / "data/out"
 
 
 def read_and_replace_txt(file_path):
@@ -72,26 +73,21 @@ def test_mialsrtk_interface(capsys):
 
 def test_niftymic_iterate_subject(capsys):
     """Test the text output of run_niftymic."""
-    with open(CONFIG_PATH, "r") as f:
-        params = json.load(f)
-    sub = "simu005"
-    config_sub = params[sub]
-    bids_layout = BIDSLayout(BIDS_DIR, validate=True)
-    masks_folder = MASKS_DIR
-    output_path = "out"
 
-    run_niftymic(
-        sub,
-        config_sub,
-        CONFIG_PATH,
-        bids_layout,
-        BIDS_DIR,
-        output_path,
-        masks_folder,
-        alpha=0.068,
-        participant_label=None,
-        use_preprocessed=False,
-        fake_run=True,
+    main_niftymic(
+        [
+            "--data_path",
+            str(BIDS_DIR),
+            "--config",
+            str(CONFIG_PATH),
+            "--masks_folder",
+            str(MASKS_DIR),
+            "--out_path",
+            str(OUT_PATH),
+            "--participant_label",
+            "simu005",
+            "--fake_run",
+        ]
     )
     captured = capsys.readouterr()
     niftymic = read_and_replace_txt(FILE_DIR / "output/niftymic_out.txt")
@@ -109,24 +105,20 @@ def test_niftymic_interface(capsys):
 
 def test_svrtk_iterate_subject(capsys):
     """Test the text output of run_svrtk."""
-    with open(CONFIG_PATH, "r") as f:
-        params = json.load(f)
-    sub = "simu005"
-    config_sub = params[sub]
-    bids_layout = BIDSLayout(BIDS_DIR, validate=True)
-    masks_folder = MASKS_DIR
-    output_path = "out"
-
-    run_svrtk(
-        sub,
-        config_sub,
-        CONFIG_PATH,
-        bids_layout,
-        BIDS_DIR,
-        output_path,
-        masks_folder,
-        participant_label=None,
-        fake_run=True,
+    main_svrtk(
+        [
+            "--data_path",
+            str(BIDS_DIR),
+            "--config",
+            str(CONFIG_PATH),
+            "--masks_folder",
+            str(MASKS_DIR),
+            "--out_path",
+            str(OUT_PATH),
+            "--participant_label",
+            "simu005",
+            "--fake_run",
+        ]
     )
     captured = capsys.readouterr()
     svrtk = read_and_replace_txt(FILE_DIR / "output/svrtk_out.txt")
@@ -143,26 +135,22 @@ def test_svrtk_interface(capsys):
 
 def test_nesvor_source_iterate_subject(capsys):
     """Test the text output of run_nesvor_from_config."""
-    with open(CONFIG_PATH, "r") as f:
-        params = json.load(f)
-    sub = "simu005"
-    config_sub = params[sub]
-    bids_layout = BIDSLayout(BIDS_DIR, validate=True)
-    masks_folder = MASKS_DIR
-    output_path = "out"
-
-    run_nesvor(
-        sub,
-        config_sub,
-        bids_layout,
-        BIDS_DIR,
-        output_path,
-        masks_folder,
-        participant_label=None,
-        target_res=[1.1],
-        single_precision=False,
-        config=params,
-        fake_run=True,
+    main_nesvor(
+        [
+            "--data_path",
+            str(BIDS_DIR),
+            "--config",
+            str(CONFIG_PATH),
+            "--masks_folder",
+            str(MASKS_DIR),
+            "--out_path",
+            str(OUT_PATH),
+            "--participant_label",
+            "simu005",
+            "--target_res",
+            "1.1",
+            "--fake_run",
+        ]
     )
     captured = capsys.readouterr()
     nesvor = read_and_replace_txt(FILE_DIR / "output/nesvor_source_out.txt")
@@ -179,25 +167,22 @@ def test_nesvor_source_interface(capsys):
 
 def test_nesvor_docker_iterate_subject(capsys):
     """Test the text output of run_nesvor_from_config."""
-    with open(CONFIG_PATH, "r") as f:
-        params = json.load(f)
-    sub = "simu005"
-    config_sub = params[sub]
-    sub_ses_dict = iter_dir(BIDS_DIR, add_run_only=True)
-    masks_folder = MASKS_DIR
-    output_path = "out"
-
-    run_nesvor_docker(
-        sub,
-        config_sub,
-        sub_ses_dict,
-        BIDS_DIR,
-        output_path,
-        masks_folder,
-        participant_label=None,
-        target_res=[1.1],
-        config=params,
-        fake_run=True,
+    main_nesvor_docker(
+        [
+            "--data_path",
+            str(BIDS_DIR),
+            "--config",
+            str(CONFIG_PATH),
+            "--masks_folder",
+            str(MASKS_DIR),
+            "--out_path",
+            str(OUT_PATH),
+            "--participant_label",
+            "simu005",
+            "--target_res",
+            "1.1",
+            "--fake_run",
+        ]
     )
     captured = capsys.readouterr()
     nesvor = read_and_replace_txt(FILE_DIR / "output/nesvor_docker_out.txt")
